5b94429d08ae7f90ac20fe75dfb500ed2301f690
Darren Clarke <darren@redaranj.com>
2026-05-04 10:17:58 +0200

    More Gradle version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ each license.
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("com.android.library") version "9.1.1" apply false
+    id("com.android.library") version "9.2.0" apply false
     alias(libs.plugins.kotlin.compose) apply false
     kotlin("jvm") version "2.2.10" apply false
     kotlin("plugin.serialization") version "2.2.10" apply false
